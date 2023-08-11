b0d167b864009cde9266d739660715891d7c40d9
abrolab <olaideibrahim36@gmail.com>
2023-08-10 23:34:17 -0700

    Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,14 +6,15 @@
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
+
 int check_cycle(listint_t *list)
-{	
-	listint_t *slow = list
+{
+	listint_t *slow = list;
 	listint_t *fast = list;
-
-	if (!list)
-		return (0);
-
+	
+		if (!list)
+			return (0);
+	
 	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
